7c1eb7779e2c00124742a27335e4451796bb4701
Susie Wang <SWang@lcs.on.ca>
2024-10-28 17:16:32 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Susie Wang on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
